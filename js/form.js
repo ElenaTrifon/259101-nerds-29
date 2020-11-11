2a454daca768e6d2feaaf78e4343855a426fe4ca
Elena Trifonova <markiigx81@gmail.com>
2020-11-11 23:55:21 +0300

    Защита личного проекта

---
+++ js/form.js
@@ -0,0 +1,57 @@
+const loginLink = document.querySelector(".footer-button");
+const loginPopup = document.querySelector(".modal-login");
+const loginClose = loginPopup.querySelector(".modal-close");
+const loginForm = loginPopup.querySelector(".login-form");
+const loginLogin = loginPopup.querySelector(".login-user");
+const loginEmail = loginPopup.querySelector(".login-email");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+
+  if (storage) {
+    loginLogin.value = storage;
+    loginEmail.focus();
+  } else {
+    loginLogin.focus();
+  }
+
+  loginLogin.focus();
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginEmail.value) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-error");
+    loginPopup.offsetWidth = loginPopup.offsetWidth;
+    loginPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+    }
+  }
+});
